5e40a37b5ba3f7cc4f5d7d324f683779e72df2b7
shin <cyshin1031@naver.com>
2024-12-13 19:11:02 +0900

    new line

--- src/main/java/com/example/ch1/Ch1Application.java
+++ src/main/java/com/example/ch1/Ch1Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ch1Application {
 	public static void main(String[] args) {
+		System.out.println("new");
 		SpringApplication.run(Ch1Application.class, args);
 	}
 }
