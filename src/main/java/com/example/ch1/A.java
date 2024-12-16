57a7afb35c6392eceefa0cc297b56d587fb01599
shin <cyshin1031@naver.com>
2024-12-16 14:55:41 +0900

    A class commit

--- src/main/java/com/example/ch1/A.java
+++ src/main/java/com/example/ch1/A.java
@@ -3,6 +3,8 @@ package com.example.ch1;
 public class A {
     public void test(){
         System.out.println("A test");
+        System.out.println("A test2");
+        System.out.println("A test3");
 
     }
 }
---
+++ src/main/java/com/example/ch1/B.java
@@ -0,0 +1,8 @@
+package com.example.ch1;
+
+public class B {
+    public void test(){
+        System.out.println("B test");
+
+    }
+}
